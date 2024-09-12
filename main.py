69564457516ee494b7e16a8d1d583c33451cf420
Casey Stumpf <stumpf80369@gmail.com>
2024-09-12 12:37:29 -0600

    update registered user layout

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ from history import walletCoinListBuilder, runWallets
 def main():
     root = Tk()
     ta.startApp(root)
-    # ne.newEntry()
-    # history.nameListBuilder()
-    #history.runNames()
-    #history.searchDB()
-    #history.nameListApp()
-    #print(type("QWERTY", "Dvorak", "Colemak", "Workman"))
-    # runWallets("Casey Stumpf", "Main")
 
 
 if __name__ == '__main__':
--- tkNewUser.py
+++ tkNewUser.py
@@ -1,13 +1,18 @@
+from datetime import datetime
+import json
 from tkinter import *
-import time
-import threading
 from tkinter import messagebox
+
+import coingeckoCalls
+from pyMongoDB import db
+
+currentDateAndTime = datetime.now()
 
 
 def runNewUser(root):
     newUserWindow = Toplevel(root)
     newUserWindow.title("New User's Crypto Counter")
-    newUserWindow.geometry("600x300")
+    newUserWindow.geometry("600x500")
 
     # nameLabel = Label(newUserWindow, text="Enter Name (First Last)" + "\n" + "eg: John Doe", font=("Helvetica", 12))
     # nameLabel.pack(pady=10)
@@ -17,10 +22,10 @@ def runNewUser(root):
     def display_info():
         # Get input from entry fields
         customer_name = entry_name.get()
-        customer_email = entry_email.get()
+        customer_email = entry_wallet.get()
 
-        # Display the customer information
-        messagebox.showinfo("Customer Information", f"Name: {customer_name}\nWallet: {customer_email}")
+        # # Display the customer information
+        # messagebox.showinfo("Customer Information", f"Name: {customer_name}\nWallet: {customer_email}")
 
     # Labels and Entry fields
     label_name = Label(newUserWindow, text="Enter Name (First Last)" + "\n" + "eg: John Doe", font=("Helvetica", 12))
@@ -28,17 +33,101 @@ def runNewUser(root):
     entry_name = Entry(newUserWindow, width=40)
     entry_name.pack(pady=5)
 
-    label_email = Label(newUserWindow, text="Enter Wallet Name" + "\n" + "eg: Main, Binance", font=("Helvetica", 12))
-    label_email.pack(pady=5)
-    entry_email = Entry(newUserWindow, width=40)
-    entry_email.pack(pady=5)
+    label_wallet = Label(newUserWindow, text="Enter Wallet Name" + "\n" + "eg: Main, Binance", font=("Helvetica", 12))
+    label_wallet.pack(pady=5)
+    entry_wallet = Entry(newUserWindow, width=40)
+    entry_wallet.pack(pady=5)
+
+    label_coin = Label(newUserWindow, text="Enter Coin" + "\n" + "eg: bitcoin, ethereum", font=("Helvetica", 12))
+    label_coin.pack(pady=5)
+    entry_coin = Entry(newUserWindow, width=40)
+    entry_coin.pack(pady=5)
+
+    label_amount = Label(newUserWindow, text="Enter Amount of Coin" + "\n" + "eg: 3.5673235", font=("Helvetica", 12))
+    label_amount.pack(pady=5)
+    entry_amount = Entry(newUserWindow, width=40)
+    entry_amount.pack(pady=5)
+
+    def submitUser():
+        if (entry_name.get() == ""):
+            messagebox.showinfo("Error", "Please enter Name")
+            display_info()
+
+        if (entry_wallet.get() == ""):
+            messagebox.showinfo("Error", "Please enter Wallet")
+            display_info()
+
+        if (entry_coin.get() == ""):
+            messagebox.showinfo("Error", "Please enter Coin")
+            display_info()
+
+        if (entry_amount.get() == ""):
+            messagebox.showinfo("Error", "Please enter Amount of Coin")
+            display_info()
+
+        # check all values are inputted
+        if (entry_name.get() != "" and entry_wallet.get() != "" and entry_coin.get() != "" and entry_amount.get() != ""):
+            # check that coin is a valid coin
+            if coinChecker(entry_coin.get().lower()):
+                if amountChecker(entry_amount.get()):
+
+                    # Values
+                    customerName = entry_name.get()
+                    customerCoin = entry_coin.get()
+                    customerAmount = entry_amount.get()
+
+                    # building total amount from coingecko
+                    coinValue, totalAmount = amountBuilder(customerCoin=entry_coin.get(), customerAmount=entry_amount.get())
+
+                    # adding values to dictionary and appending to list
+                    post = {"customerName": customerName, "customerCoin": customerCoin, "coinValue": coinValue,
+                            "customerAmount": customerAmount, "totalAmount": totalAmount,
+                            "date&time": currentDateAndTime}
+                    db.collection.insert_one(post)
 
 
+    # coin checker
+    customerListDict = []
+    geckoListCheck = coingeckoCalls.coinsList()
+    dictionaryListCheck = json.loads(geckoListCheck)
+
+    def coinChecker(customerInput):
+        coinCheck = False
+        for dictionary in dictionaryListCheck:
+            if dictionary['id'] == str(customerInput):
+                coinCheck = True
+
+        if not coinCheck:
+            messagebox.showinfo("Error", "Coin entered is not valid")
+            display_info()
+        return coinCheck
+
+    # amount checker
+    def amountChecker(customerInput):
+        amountCheck = False
+        # try making float if not ask for correct input
+        try:
+            customerInput = float(customerInput)
+            amountCheck = True
+        except ValueError:
+            messagebox.showinfo("Error", "\n" + "Enter Valid Amount of Coin" + "\n" + "eg: 3.5673235")
+            display_info()
+
+    # amount builder
+    def amountBuilder(customerCoin, customerAmount):
+        # coin look up for other information
+        geckoListLook = coingeckoCalls.coinRaw(customerCoin)
+        dictionaryListLook = json.loads(geckoListLook)
+
+        # look at usd value and build total
+        coinValue = dictionaryListLook[customerCoin]['usd']
+        customerAmount = float(float(customerAmount) * float(coinValue))
+        return float(coinValue), float(customerAmount)
 
 
 
     # Button to trigger the display function
-    submit_button = Button(newUserWindow, text="Submit", command=display_info)
+    submit_button = Button(newUserWindow, text="Submit", command=submitUser)
     submit_button.pack(pady=10)
 
     close_button = Button(newUserWindow, text="Close", command=newUserWindow.destroy)
